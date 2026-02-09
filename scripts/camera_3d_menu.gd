a7990f26efbb7aae94d9b524ee4ed3d534fc0673
Bello <jm.soum@outlook.fr>
2026-02-09 20:17:26 +0100

    flm2

---
+++ scripts/camera_3d_menu.gd
@@ -0,0 +1,11 @@
+extends Camera3D
+
+var origin_position = position
+var intensity = 0.1
+
+func _process(_delta: float) -> void:
+	var position_x = origin_position.x + randf_range(-intensity, intensity)
+	var position_y = origin_position.y + randf_range(-intensity, intensity)
+	var position_z = origin_position.z + randf_range(-intensity, intensity)
+	position = Vector3(position_x, position_y, position_z)
+	
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,6 +1,37 @@
 extends Node3D
 
 @onready var main := $".."
+@onready var wall1 := $SubViewportContainer/SubViewport/Wall
+@onready var wall2 := $SubViewportContainer/SubViewport/Wall2
+@onready var quit_button := $Node2D/Quit
+@onready var red_flash := $Node2D/RedFlash
+@onready var node_2d := $Node2D
+@onready var camera := $SubViewportContainer/SubViewport/Camera3D
 
 func _on_enter_pressed() -> void:
+	for i in range(100):
+		node_2d.position.x += 5
+		camera.origin_position.z += 0.005
+		camera.origin_position.x -= 0.005
+		await get_tree().process_frame
+		$Timer.start()
+
+func _process(_delta: float) -> void:
+	wall1.position.y += 0.3
+	wall2.position.y += 0.3
+	if wall1.position.y >= 20:
+		wall1.position.y -= 20
+	if wall2.position.y >= 20:
+		wall2.position.y -= 20
+	
+	if quit_button.is_hovered():
+		red_flash.visible = true
+	else:
+		red_flash.visible = false
+
+func _on_timer_timeout() -> void:
+	$ColorRect.visible = true
+	$Timer2.start()
+
+func _on_timer_2_timeout() -> void:
 	main.start_intro()
